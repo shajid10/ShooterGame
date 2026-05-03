164352ff99661da2431d34f275f8696093a970b6
Shajid Hossain <shajidh82@gmail.com>
2026-05-03 15:02:19 +0600

    Added bullet despawning mechanics after 3 seconds

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-    private float speed = 12f;
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float timeToLive = 3f;
+
+    private void Start() {
+        Destroy(gameObject, timeToLive);
+    }
 
     private void FixedUpdate() {
         transform.position += transform.forward * speed * Time.deltaTime;
